d51df412955373367ddfff988da558f21704974e
15328628 <15328628@qq.com>
2017-12-29 20:58:58 +0800

    add

--- Demo.php
+++ Demo.php
@@ -18,6 +18,7 @@ echo '更新了：'.$db->num.' 条记录';
 /**
 * 二、新增操作
 **/
+/*
 $create_time = time();
 $update_time = time();
 
@@ -25,5 +26,33 @@ $sql = "INSERT  student SET name = '朱老师', email='test@qq.com', course ='js
 
 $db->exec($sql);
 echo '成功新增了：'.$db->num.' 条记录, 新增主键id是' . $db->insertId;
+*/
 
-//查询
+
+/**
+* 三、删除操作
+**/
+/*
+$sql = "DELETE FROM student where id = 6";
+
+$db->exec($sql);
+echo '成功删除了：'.$db->num.' 条记录, 删除的主键id是' . $db->insertId;
+*/
+
+
+
+/**
+* 四、查询单条
+**/
+
+$sql = "SELECT * From student where grade > 56";
+$row = $db->fetch($sql);
+var_dump($row);
+die;
+
+/**
+* 五、查询多条
+**/
+$sql = "SELECT * FROM student";
+$rows = $db->fetchAll($sql);
+var_dump($rows);
